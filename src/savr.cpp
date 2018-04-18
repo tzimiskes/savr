dc8ac6f94ea8d84d697e2184b35d2d3d980cf621
jonathonl <jonathonl@users.noreply.github.com>
2018-04-18 16:43:47 -0400

    Adds info fields to variants data frame.

--- src/savr.cpp
+++ src/savr.cpp
@@ -7,37 +7,6 @@
 #include <string>
 #include <list>
 
-//Rcpp::DataFrame data_frame_from_list( Rcpp::List obj ){
-//             bool use_default_strings_as_factors = true ;
-//             bool strings_as_factors = true ;
-//             int strings_as_factors_index = -1 ;
-//             R_xlen_t n = obj.size() ;
-//             CharacterVector names = obj.attr( "names" ) ;
-//             if( !names.isNULL() ){
-//                 for( int i=0; i<n; i++){
-//                     if( names[i] == "stringsAsFactors" ){
-//                         strings_as_factors_index = i ;
-//                         use_default_strings_as_factors = false ;
-//                         if( !as<bool>(obj[i]) ) strings_as_factors = false ;
-//                         break ;
-//                     }
-//                 }
-//             }
-//             if( use_default_strings_as_factors )
-//                 return DataFrame_Impl(obj) ;
-//             SEXP as_df_symb = Rf_install("as.data.frame");
-//             SEXP strings_as_factors_symb = Rf_install("stringsAsFactors");
-// 
-//             obj.erase(strings_as_factors_index) ;
-//             names.erase(strings_as_factors_index) ;
-//             obj.attr( "names") = names ;
-//             Shield<SEXP> call( Rf_lang3(as_df_symb, obj, wrap( strings_as_factors ) ) ) ;
-//             SET_TAG( CDDR(call),  strings_as_factors_symb ) ;
-//             Shield<SEXP> res( Rcpp_eval( call ) ) ;
-//             DataFrame_Impl out( res ) ;
-//             return out ;
-// 
-//         }
 
 //' Reads headers and sample ID list from SAV file.
 //' @param path SAV file path.
@@ -104,6 +73,7 @@ Rcpp::List read_sav_region(const std::string& file_path, const std::string& chro
   std::vector<std::string> alt_alleles(nrows);
 
   Rcpp::NumericMatrix geno_data(nrows, ncols);
+  std::vector<std::vector<std::string>> info_columns(file.info_fields().size(), std::vector<std::string>(nrows));
 
   std::size_t i = 0;
   for (auto it = tmp_vars.begin(); it != tmp_vars.end(); ++it,++i)
@@ -112,6 +82,12 @@ Rcpp::List read_sav_region(const std::string& file_path, const std::string& chro
     positions[i] = it->position();
     ref_alleles[i] = it->ref();
     alt_alleles[i] = it->alt();
+    
+    std::size_t j = 0;
+    for (auto jt = file.info_fields().begin(); jt != file.info_fields().end(); ++jt,++j)
+    {
+      info_columns[j][i] = it->prop(*jt);
+    }
 
     auto jt_end = it->data().end();
     for (auto jt = it->data().begin(); jt != jt_end; ++jt)
@@ -122,8 +98,13 @@ Rcpp::List read_sav_region(const std::string& file_path, const std::string& chro
   
   Rcpp::List result;
   
-  Rcpp::List columns(4);
-  std::vector<std::string> column_names = {"chrom", "pos", "ref", "alt"};
+  std::vector<std::string> column_names;
+  column_names.reserve(4 + file.info_fields().size());
+  column_names = {"chrom","pos", "ref", "alt"};
+  for (auto it = file.info_fields().begin(); it != file.info_fields().end(); ++it)
+    column_names.push_back(*it);
+  
+  Rcpp::List columns(4 + file.info_fields().size());
 
   // Hack thanks to Matthew Flickinger
   columns.attr("names") = column_names;
@@ -134,6 +115,11 @@ Rcpp::List read_sav_region(const std::string& file_path, const std::string& chro
   columns[1] = std::move(positions);
   columns[2] = std::move(ref_alleles);
   columns[3] = std::move(alt_alleles);
+
+  for (std::size_t i = 0; i < file.info_fields().size(); ++i)
+  {
+    columns[i + 4] = std::move(info_columns[i]);
+  }
 
   result["variants"] = columns;
   
